2907ca09738687a590f96764bcd59acc3d0cfcb5
Jenn Youngblood <50023967+YoungbloodJenn@users.noreply.github.com>
2020-01-04 15:56:50 -0800

    Add files via upload

---
+++ Go Bag Project/js/main.js
@@ -0,0 +1,15 @@
+// /*page load alert*/
+// alert ("Welcome to Go Bag Builder - a place to prep for emergencies, disasters, and more!");
+// let question = "What is your name?";
+// var response = prompt(question);
+// alert("Hi, " + response + ". Get started with your Go Bag!");
+
+
+/*add item message. move this to the resources page*/
+
+// function addItem(){
+//     var response = prompt("What do you want to add next?");
+//     alert("OK! Let's add a " + response + "!");
+// }
+// addItem();
+
