7ecef4d10ab3083607429a38bf4ee89179e6bad9
Jenn Youngblood <50023967+YoungbloodJenn@users.noreply.github.com>
2020-01-29 16:45:15 -0800

    Final Go Bag Project
    Presented 1/29/20 
    **add progress bar!

--- Go Bag Project/js/main.js
+++ Go Bag Project/js/main.js
@@ -31,4 +31,21 @@ $('#showModal').on('click', function(){
      $('.modal-background').hide();
  });
 
+ //image carousel
+
+ var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("resourceImg");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); // Change image every 3 seconds
+}
+
     
